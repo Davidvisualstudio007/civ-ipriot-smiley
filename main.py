6f623a5e7b5c1585b182e90119bb15be82a89725
David Turangan <daudturangan@gmail.com>
2025-10-31 01:16:22 +0800

    uncomment the freeze stuff

--- main.py
+++ main.py
@@ -17,10 +17,7 @@ def main():
 
 if __name__ == '__main__':
     print("test sad")
-    ############################################################
-    # Uncomment the lines below only if you have multi-processing issues
-    # from multiprocessing import freeze_support
-    # freeze_support()
-    ############################################################
+    from multiprocessing import freeze_support
+    freeze_support()
     main()
 
